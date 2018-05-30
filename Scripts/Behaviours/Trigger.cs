acb155db3badf7fe996bd9c8d4b6a20cd0ff6118
Anthony Romrell <anthony@artisandream.com>
2018-05-30 15:38:05 -0600

    Added Generic Float and Trigger Objects

---
+++ Scripts/Behaviours/Trigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+	public FloatBase Data;
+	public FunctionBase Work;
+	
+	private void OnTriggerEnter(Collider other)
+	{
+		Work.Run(Data);
+	}
+}
