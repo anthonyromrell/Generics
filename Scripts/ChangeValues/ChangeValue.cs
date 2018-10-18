1f7d6399224fa9e874ffc07366e239a018dbd8f1
Anthony Romrell <anthonyromrell@mac.com>
2018-10-18 00:40:40 -0600

    Healthbar

---
+++ Scripts/ChangeValues/ChangeValue.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+//Made By Anthony Romrell
+[CreateAssetMenu]
+public class ChangeValue : ScriptableObject
+{
+	public FloatData Data;
+	
+	public void AddValue (FloatData obj)
+	{
+		Data.Value += obj.Value;
+	}
+	
+	public void SubtractValue (FloatData obj)
+	{
+		Data.Value -= obj.Value;
+	}
+}
